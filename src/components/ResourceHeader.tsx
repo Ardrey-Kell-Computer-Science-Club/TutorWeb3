9ee2d7d46067e4d1bfb2da1393f0b586a15e9659
Shivang Patel <shivangpatel2050@gmail.com>
2026-06-09 21:42:25 -0400

    boilerplate code delted start from fresh and make this fully working

---
+++ src/components/ResourceHeader.tsx
@@ -0,0 +1,15 @@
+export default function ResourcePageHeader() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1> Resource Page </h1>
+      <p> This is the resource page. </p>
+      <ol>
+        <ul> AP Computer Science </ul>
+        <ul> AP Computer Science Principles </ul>
+        <ul> Python Programming I Honors </ul>
+        <ul> Python Programming II Honors </ul>
+        <ul> Computer Science I Honors </ul>
+      </ol>
+    </div>
+  );
+}
---
+++ src/pages/resource.tsx
@@ -0,0 +1,9 @@
+import ResourcePageHeader from "../components/ResourceHeader";
+
+export default function ResourcePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <ResourcePageHeader />
+    </div>
+  );
+}
