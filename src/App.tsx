9ea24cf6f11ebab1d75e6c2d4aa409bbfa9b0ac4
Shivang Patel <shivangpatel2050@gmail.com>
2026-06-11 20:00:02 -0400

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-export default function AppPage() {
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import TutorPage from "./pages/FindTutor";
+import HomePage from "./pages/HomePage";
+
+// export default function AppPage() {
+//   return (
+//     <div className="app">
+//       <h1> Welcome to TutorWeb3 </h1>
+//     </div>
+//   );
+// }
+
+export default function MainAppPage() {
   return (
-    <div className="app">
-      <h1> Welcome to TutorWeb3 </h1>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/"> Home</Link>
+        <Link to="/tutor"> Tutor </Link>
+        <Link to="/resources"> Resources</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/tutor" element={<TutorPage />} />
+        <Route path="/about" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
